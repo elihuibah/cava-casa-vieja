fb8189cfc9a08fba911e0c77c5ae717cfef4a404
elihuibah <elihuibh@gmail.com>
2026-08-03 10:51:12 -0700

    Started Cava Casa Vieja web project

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <h1>RESERVA AHORA:</h1>
+      <h4>CONTACTO</h4>
+      <p>&copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+function Header() {
+  return (
+    <header>
+      <h1>Test</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Nosotros</a>
+          </li>
+          <li>
+            <a href="#">Menú</a>
+          </li>
+          <li>
+            <a href="#">Ubicación</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+          <li>
+            <a href="#">ES</a>
+          </li>
+          <li>
+            <a href="#">EN</a>
+          </li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+}
+
+export default Header;
